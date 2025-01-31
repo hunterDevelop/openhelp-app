e0f2387c89acfac8b6a8db6234ec5347e2397f2e
Alex Burm <79045503+alex-burm@users.noreply.github.com>
2025-01-31 12:01:01 +0100

    fix for docker-rr (#7)
    * - create frankenphp docker settings
    
    * - add composer install to docker-nginx settings
    - change .env.dist
    - move commands from docker-unit/env.sh to entrypoint.sh
    
    * change config/bunldes.php before composer update

--- documents/docker-rr/entrypoint.sh
+++ documents/docker-rr/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -e
-composer require baldinof/roadrunner-bundle
-composer require spiral/roadrunner-cli --dev
 
 grep -q "Baldinof\\\\RoadRunnerBundle\\\\BaldinofRoadRunnerBundle" config/bundles.php || \
     sed -i "/return \[/a \ \ \ \ Baldinof\\\\RoadRunnerBundle\\\\BaldinofRoadRunnerBundle::class => ['all' => true]," config/bundles.php
+
+composer require baldinof/roadrunner-bundle
+composer require spiral/roadrunner-cli --dev
+
 
 rr serve -c /docker-entrypoint.d/rr.yaml
 #sleep 1000
